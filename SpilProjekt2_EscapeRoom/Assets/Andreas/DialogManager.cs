15d31f905ff0393a5699b430415a283b10824cbe
Hozt1536 <andreasgamer1536@gmail.com>
2024-04-17 09:53:08 +0200

    canvas dialog (not finished 2.0)

--- SpilProjekt2_EscapeRoom/Assets/Andreas/DialogManager.cs
+++ SpilProjekt2_EscapeRoom/Assets/Andreas/DialogManager.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
 using UnityEngine;
 
 public class DialogManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class DialogueSegment
+    {
+        public string SubjectText;
+
+        [TextArea]
+        public string DialogueToPrint;
+        public bool skippable;
+
+        [Range(1f, 25f)]
+        public float LettersPerSecond;
+    }
+
+    [SerializeField] private DialogueSegment[] DialogueSegments;
+    [Space]
+    [SerializeField] private TMP_Text SubjectText;
+    [SerializeField] private TMP_Text BodyText;
+
+    private int DialogueIndex;
+    private bool PlayingDialogue;
+    private bool skip;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +38,24 @@ public class DialogManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private IEnumerator PlayDialogue(DialogueSegment segment)
+    {
+        PlayingDialogue = true;
+
+        BodyText.SetText(string.Empty);
+        SubjectText.SetText(segment.SubjectText);
+
+        float delay = 1f / segment.LettersPerSecond;
+        for (int i = 0; i < segment.DialogueToPrint.Length; i++)
+        {
+            if (skip)
+            {
+                BodyText.SetText(segment.DialogueToPrint);
+                skip = false;
+                break;
+            }
+        }
     }
 }
