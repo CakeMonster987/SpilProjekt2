673fdb77d47b734a38199f3b594a150e7e50cded
HannibalBlaabjerg <hannibal@blab.dk>
2024-05-16 11:48:11 +0200

    saf

---
+++ SpilProjekt2_EscapeRoom/Assets/Hannibal/Scripts/Cooker.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooker : MonoBehaviour
+{
+    public GameObject SkullTonic;
+
+    public GameObject GhoulsGrog;
+
+    public GameObject ZombieZest;
+
+    public GameObject bansheeBrew;
+
+    public List<Ingridients> IngridientsNeeded = new();
+
+    public List<Ingridients> currentIngridient = new();
+
+    public GameObject spawnPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
